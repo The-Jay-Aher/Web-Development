5916d10883a835e23fc8e691cf3bad7d521bc813
Jay Aher <115747060+The-Jay-Aher@users.noreply.github.com>
2022-11-07 19:10:12 +0530

    quiz-application done

--- CodeDamn/Frontend/4 - React.js + UI - Zero to Hero/4.01 - React Fundamentals/Projects/react-quiz-application/src/App.jsx
+++ CodeDamn/Frontend/4 - React.js + UI - Zero to Hero/4.01 - React Fundamentals/Projects/react-quiz-application/src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 
 export default function App() {
+	// Define a state variable here to track question status
+	const [currentIndex, setCurrentIndex] = useState(0)
+
 	const questions = [
 		{
 			questionText: 'What is the capital of France?',
@@ -40,28 +43,53 @@ export default function App() {
 		},
 	]
 
+	function handleAnswerClick(isCorrect) {
+		// Check if correct answer is pressed. (See the hint on the left)
+        if(isCorrect) {
+            setScore((score) => score + 1);
+        }
+        
+		if (currentIndex === questions.length - 1) {
+			// quiz over
+			setQuizFinished(true)
+		} else {
+			setCurrentIndex((value) => value + 1)
+		}
+	}
+
+	const [quizFinished, setQuizFinished] = useState(false)
+    const [score, setScore] = useState(0)
+	// Create a state variable here [score, setScore]
+
 	return (
 		<div className="app">
-			{false ? (
+			{quizFinished ? (
+				/* Change this hardcoded 1 to state variable score else */
 				<div className="score-section">
-					You scored 1 out of {questions.length}
+					You scored {score} out of {questions.length}
 				</div>
 			) : (
 				<>
 					<div className="question-section">
 						<div className="question-count">
-							<span>Question 1</span>/{questions.length}
+							<span>Question {currentIndex + 1}</span>/{questions.length}
 						</div>
-						{/* HINT: You can access first question using questions[0] */}
 						<div className="question-text">
-							This is where the question text should go
+							{questions[currentIndex].questionText}
 						</div>
 					</div>
 					<div className="answer-section">
-						<button>Answer 1</button>
-						<button>Answer 2</button>
-						<button>Answer 3</button>
-						<button>Answer 4</button>
+						{questions[currentIndex].answerOptions.map((answer) => {
+							// Add onClick listener to this button
+							return (
+								<button
+									onClick={() => handleAnswerClick(answer.isCorrect)}
+									key={answer.answerText}
+								>
+									{answer.answerText}
+								</button>
+							)
+						})}
 					</div>
 				</>
 			)}
